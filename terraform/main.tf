917de0d0432db341f75d3a00779c59e740bded82
unknown <ouse4481@gmail.com>
2026-03-31 18:03:59 +0200

    Fix problèmes version 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
-# 1. Groupe de ressources (placé en premier pour la lisibilité)
-resource "azurerm_resource_group" "example" {
-    name     = "rg-app-ousmane"
-    location = "northeurope"
-}
-
-# 2. Module Réseau
-module "network" {
+# 1. Module Réseau
+module "network"{
     source = "./modules/network"
 
     network_name     = "vnet-ousmane-app"
@@ -15,17 +9,18 @@ module "network" {
     depends_on = [azurerm_resource_group.example]
 }
 
-# 3. Module Application Web
-module "appservice" {
+# 2. Module Application Web
+module "appservice"{
     source = "./modules/appservices"
 
     app_service_plan_name     = "plan-ousmane-dev"
     app_service_plan_location = azurerm_resource_group.example.location
     app_service_plan_rg       = azurerm_resource_group.example.name
-    app_service_plan_sku      = "F1"
+    app_service_plan_tiers    = "Free"      # Change 'Standard' par 'Free'
+    app_service_plan_sku      = "F1"        # Change 'S1' par 'F1'
 
     # ATTENTION : Ce nom génère l'URL de ton site, il doit être unique !
-    app_service_name     = "webapp-ousmane-2026"
+    app_service_name     = "webapp-ousmane-2026" 
     app_service_location = azurerm_resource_group.example.location
     app_service_rg       = azurerm_resource_group.example.name
     app_service_tag_env  = "dev"
@@ -33,21 +28,27 @@ module "appservice" {
     depends_on = [azurerm_resource_group.example]
 }
 
-# 4. Module Base de données PostgreSQL
-module "postgre" {
+# 3. Module Base de données PostgreSQL
+module "postgre"{
     source = "./modules/data"
 
     # ATTENTION : Ce nom génère l'URL de la base, il doit être unique !
-    postgre_name                         = "pg-ousmane-2026"
+    postgre_name                         = "pg-ousmane-2026" 
     postgre_location                     = azurerm_resource_group.example.location
     postgre_rg                           = azurerm_resource_group.example.name
     postgre_administrator_login          = var.administrator_login
     postgre_administrator_login_password = var.administrator_login_password
-    postgre_sku_name                     = "GP_Standard_D4s_v3"  # Nouveau format SKU v3+
-    postgre_version                      = "16"                   # 9.6 est déprécié, 16 = LTS actuel
-    postgre_storage_mb                   = 640000                 # Nombre entier, pas une string
+    postgre_sku_name                     = "GP_Gen5_4"
+    postgre_version                      = "16"
+    postgre_storage_mb                   = "640000"
 
     depends_on = [azurerm_resource_group.example]
+}
+
+# 4. Groupe de ressources de l'application
+resource "azurerm_resource_group" "example" {
+    name     = "rg-app-ousmane"
+    location = "northeurope"
 }
 
 # 5. Variables de sortie
--- terraform/modules/appservices/appservice.tf
+++ terraform/modules/appservices/appservice.tf
@@ -1,31 +1,44 @@
-# azurerm_app_service_plan  →  azurerm_service_plan (depuis AzureRM v3)
-resource "azurerm_service_plan" "dev_app_service_plan" {
+# Création du Plan App Service (La machine sous-jacente qui héberge le site)
+resource "azurerm_app_service_plan" "dev_app_service_plan" {
     name                = var.app_service_plan_name
     location            = var.app_service_plan_location
     resource_group_name = var.app_service_plan_rg
-    os_type             = "Windows"
-    sku_name            = var.app_service_plan_sku
+
+    sku {
+        tier = var.app_service_plan_tiers
+        size = var.app_service_plan_sku
+    }
 }
 
-# azurerm_app_service  →  azurerm_windows_web_app (depuis AzureRM v3)
-resource "azurerm_windows_web_app" "dev_app_service" {
+# Création de l'App Service (Le site web en lui-même)
+resource "azurerm_app_service" "dev_app_service" {
     name                = var.app_service_name
     location            = var.app_service_location
     resource_group_name = var.app_service_rg
-    service_plan_id     = azurerm_service_plan.dev_app_service_plan.id
-
-    app_settings = {
-        "SOME_KEY" = "some-value"
-    }
+    app_service_plan_id = azurerm_app_service_plan.dev_app_service_plan.id
 
     site_config {
-        application_stack {
-            current_stack  = "dotnet"
-            dotnet_version = "v4.0"
-        }
+        dotnet_framework_version = "v4.0"
+        scm_type                 = "LocalGit"
     }
 
     tags = {
         environment = var.app_service_tag_env
     }
+
+    app_settings = {
+        "SOME_KEY" = "some-value"
+    }
+
+    connection_string {
+        name  = "Database"
+        type  = "PostgreSQL" 
+        value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+    }
+}
+
+# La valeur de sortie qui remontera jusqu'au main.tf principal
+output "app_service_url" {
+    value       = azurerm_app_service.dev_app_service.default_site_hostname
+    description = "URL to connect to App Service"
 }
